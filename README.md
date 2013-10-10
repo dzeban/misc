f4509cc4b3a084e3f5f6121e8389ee185531a13b
Alex Dzyoba <avd@reduct.ru>
2013-10-10 11:38:43 +0400

    Rewritten fint with bash functions, updated README.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ misc
 ====
 
 Miscellaneous goodies
+
+* fint.sh - Fedora installer script. Does some basic setup from minimal setup.
--- fint.sh
+++ fint.sh
@@ -12,51 +12,111 @@ libX11-devel libXinerama-devel libxcb-devel  # For dwm
 freetype-infinality fontconfig-infinality    # Infinality patches
 )
 
-# ==============================================
-#                   REPOS
-# ==============================================
+function usage
+{
+    cat << USAGE
+    fint.sh - Fedora Installer script by avd.
 
-# Enable RPM Fusion repo
-yum -y install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    usage: $0 <options>
 
-# Enable Russian Fedora repo
-yum -y install http://mirror.yandex.ru/fedora/russianfedora/russianfedora/free/fedora/russianfedora-free-release-stable.noarch.rpm http://mirror.yandex.ru/fedora/russianfedora/russianfedora/nonfree/fedora/russianfedora-nonfree-release-stable.noarch.rpm
+    OPTIONS:
+    -h  Prints this nice message
+    -r  Repositories setup
+    -s  Install useful software
+    -d  Install and configure dwm and dmenu
+    -w  Configure window manager (adds exec in .xinitrc)
+    -c  Do various system configs
 
-# Enable infinality repo
-yum -y install http://www.infinality.net/fedora/linux/infinality-repo-1.0-1.noarch.rpm
+    If no options are given - execute all operations.
+USAGE
+}
 
-# ==============================================
-#                   SOFTWARE
-# ==============================================
+function repo_setup 
+{
+    # Enable RPM Fusion repo
+    yum -y install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-# Install software
-yum -y install ${PKGS[*]}
+    # Enable Russian Fedora repo
+    yum -y install http://mirror.yandex.ru/fedora/russianfedora/russianfedora/free/fedora/russianfedora-free-release-stable.noarch.rpm http://mirror.yandex.ru/fedora/russianfedora/russianfedora/nonfree/fedora/russianfedora-nonfree-release-stable.noarch.rpm
 
-# Install custom dwm from my github dwm mirror
-cd .. # to clone into ../misc (i.e. ~/dev/dwm)
-git clone https://github.com/dzeban/dwm.git
-cd dwm
-make clean install
+    # Enable infinality repo
+    yum -y install http://www.infinality.net/fedora/linux/infinality-repo-1.0-1.noarch.rpm
+}
 
-# Install dmenu
-cd .. # to clone into ../misc (i.e. ~/dev/dmenu)
-git clone http://git.suckless.org/dmenu
-cd dmenu
-make clean install
+function soft_install
+{
+    yum -y install ${PKGS[*]}
+}
 
-cd ../misc # Return to original directory
+function dwm_install
+{
+    # Install custom dwm from my github dwm mirror
+    cd ..
+    git clone https://github.com/dzeban/dwm.git
+    cd dwm
+    make clean install
 
-# ==============================================
-#                CONFIGURATION
-# ==============================================
+    cd ../misc # Return to original directory
+}
 
-# Set dwm as window manager for root and avd (What a nice double piping!)
-echo "#!/bin/bash" >> ~/.xinitrc >> /home/avd/.xinitrc
-echo "exec dwm"    >> ~/.xinitrc >> /home/avd/.xinitrc
+function dmenu_install
+{
+    # Install dmenu
+    cd ..
+    git clone http://git.suckless.org/dmenu
+    cd dmenu
+    make clean install
 
-# Set dwm as default session in SLIM config
-sed -i 's/sessions.*/sessions	dwm/' /etc/slim.conf
+    cd ../misc # Return to original directory
+}
 
-# Make init 5 level as default
-ln -sf /lib/systemd/system/graphical.target /etc/systemd/system/default.target
+function wm_setup
+{
+    # Set dwm as window manager for root and avd (What a nice double piping!)
+    echo "#!/bin/bash" >> ~/.xinitrc >> /home/avd/.xinitrc
+    echo "exec dwm"    >> ~/.xinitrc >> /home/avd/.xinitrc
+}
 
+function dm_setup
+{
+    # Set dwm as default session in SLIM config
+    sed -i 's/sessions.*/sessions	dwm/' /etc/slim.conf
+}
+
+function sys_config
+{
+    # Make init 5 level as default
+    ln -sf /lib/systemd/system/graphical.target /etc/systemd/system/default.target
+}
+
+function run_all
+{
+    repo_setup
+    soft_install
+    dwm_install
+    dmenu_install
+    wm_setup
+    dm_setup
+    sys_config
+}
+
+# Exit on error in any script command
+set -e
+
+if [ $# -eq 0 ]; then
+    run_all
+fi
+
+while getopts “hrsdmwc” OPTION
+do
+    case $OPTION in
+        h) usage; exit 0;;
+        r) repo_setup   ;;
+        s) soft_install ;;
+        d) dwm_install  ;;
+        m) dwm_install  ;;
+        w) wm_setup     ;;
+        c) sys_config   ;;
+        ?) usage; exit 1;;
+    esac
+done
